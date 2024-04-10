7949a19d6f30d9855fab997f3448659af4ae8ca5
Excellence KAWEJ <19ka201@esisalama.org>
2024-04-10 13:48:53 +0200

    modifying the text size and the step action needed

--- app/src/main/java/com/xcelk/lemonade/MainActivity.kt
+++ app/src/main/java/com/xcelk/lemonade/MainActivity.kt
@@ -26,8 +26,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.xcelk.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -59,6 +62,12 @@ fun LemonadePage(modifier: Modifier = Modifier, onClick: () -> Unit) {
         3 -> R.string.lemon_glass_content_desc
         else -> R.string.empty_glass_content_desc
     }
+    val step = when(currentStep){
+        1 -> R.string.step1_action
+        2 -> R.string.step2_action
+        3 -> R.string.step3_action
+        else -> R.string.step4_action
+    }
     val clickNbr = (2..4).random()
     var clickedTimes = 0
     Column(modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
@@ -67,12 +76,14 @@ fun LemonadePage(modifier: Modifier = Modifier, onClick: () -> Unit) {
                 modifier
                     .background(Color(0xFFf9e44c))
                     .fillMaxWidth()
-                    .fillMaxHeight())
+                    .fillMaxHeight(), fontSize = 32.sp, fontWeight = FontWeight.SemiBold, textAlign = TextAlign.Center
+            )
         }
         Image(painter = painterResource(id = imageId), contentDescription = stringResource(id = strDesc),
             modifier
                 .wrapContentSize()
                 .weight(0.75f)
+
                 .background(Color(0xFFc3ecd2)).clickable {
                     clickedTimes++
                     if (currentStep == 4){
@@ -85,10 +96,10 @@ fun LemonadePage(modifier: Modifier = Modifier, onClick: () -> Unit) {
                         currentStep++
                     }
                 })
-        Text(text = stringResource(id = strDesc),
-            modifier
+        Text(text = stringResource(id = step),
+            modifier = Modifier
                 .weight(0.25f)
-                .wrapContentSize(), )
+                .wrapContentSize(), fontSize = 18.sp)
     }
 }
 
